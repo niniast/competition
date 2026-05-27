0b951bfc60c61160fcb342b03b95afd022f535ee
niniast <ninia.sturua14@gmail.com>
2026-05-28 02:08:50 +0400

    დავიწყე მობ ვერსია

---
+++ assets/css/main.js
@@ -0,0 +1,7 @@
+/*მობილურში ბურგერ მენიუ*/
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
