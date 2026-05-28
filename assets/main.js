8c9a95602e30ae68d68ae023f04524e677bb6a92
niniast <ninia.sturua14@gmail.com>
2026-05-28 22:46:43 +0400

    ვევწალე ფუტერს მაგრამ უშედეგოდ

--- assets/main.js
+++ assets/main.js
@@ -5,3 +5,16 @@ const nav = document.querySelector(".nav");
 burger.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
+
+/*ფუტერი ჩამოსაშლელი*/
+const footerDropdowns = document.querySelectorAll(
+    ".footer-column:not(.footer-contact)",
+);
+
+footerDropdowns.forEach((item) => {
+    const heading = item.querySelector(".footer-heading");
+
+    heading.addEventListener("click", () => {
+        item.classList.toggle("active");
+    });
+});
